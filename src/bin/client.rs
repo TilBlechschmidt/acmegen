21fa862fb9b782d4c29daaa305dcdc5c70e13c19
Til Blechschmidt <til@blechschmidt.de>
2023-01-13 12:28:28 +0100

    Add persist option to make TF acme-dns provider happy

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -2,12 +2,16 @@ use acmegen::Claims;
 use hmac::{Hmac, Mac};
 use jwt::SignWithKey;
 use serde::{Deserialize, Serialize};
+use serde_json::json;
 use sha2::Sha256;
-use std::io;
+use std::{io, path::PathBuf};
 
 #[derive(Serialize, Deserialize)]
 struct Input {
     secret: String,
+
+    #[serde(default)]
+    persist: Option<PathBuf>,
 
     #[serde(flatten)]
     token: Claims,
@@ -16,7 +20,7 @@ struct Input {
 #[derive(Serialize, Deserialize)]
 struct Output {
     #[serde(rename = "allowfrom")]
-    allow_from: Vec<String>,
+    allow_from: Option<String>,
 
     fulldomain: String,
     subdomain: String,
@@ -27,8 +31,15 @@ struct Output {
 
 fn main() {
     let input: Input = serde_json::from_reader(io::stdin()).expect("invalid json input");
-    let key: Hmac<Sha256> = Hmac::new_from_slice(input.secret.as_bytes()).unwrap();
-    let token_str = input.token.clone().sign_with_key(&key).unwrap();
+
+    let key: Hmac<Sha256> =
+        Hmac::new_from_slice(input.secret.as_bytes()).expect("failed to create HMAC");
+
+    let token_str = input
+        .token
+        .clone()
+        .sign_with_key(&key)
+        .expect("failed to sign token");
 
     let output = Output {
         allow_from: input.token.allowed_origins,
@@ -38,5 +49,11 @@ fn main() {
         password: token_str,
     };
 
-    serde_json::to_writer(io::stdout(), &output).unwrap();
+    serde_json::to_writer(io::stdout(), &output).expect("failed to write to stdout");
+
+    if let Some(path) = input.persist {
+        let storage = serde_json::to_string(&json!({ output.fulldomain.clone(): output }))
+            .expect("failed to serialize to string");
+        std::fs::write(path, storage).expect("failed to write to storage");
+    }
 }
